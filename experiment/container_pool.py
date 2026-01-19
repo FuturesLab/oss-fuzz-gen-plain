7a47961a8688bffb556b4af6a7e04b043e75e732
gabe-sherman <gabesherman6@gmail.com>
2026-01-19 09:22:33 -0700

    fixed bug in container pool that was causing containers to be shared across processes

--- experiment/container_pool.py
+++ experiment/container_pool.py
@@ -2,32 +2,30 @@
 
 import logging
 from dataclasses import dataclass
-from multiprocessing import Lock, Value
-from typing import Any, Optional
+from multiprocessing import Manager, Queue
+from typing import Optional
 
-from experiment import oss_fuzz_checkout
-from experiment.benchmark import Benchmark
-from tool.container_tool import ProjectContainerTool
+from project.benchmark import Benchmark
+from utils import oss_fuzz_checkout
+from execution.container_tool import ProjectContainerTool
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
 class ContainerPair:
     address_container: ProjectContainerTool
     coverage_container: ProjectContainerTool
-    in_use: Any
-
 
 class ContainerPool:
     """This class represents a pool of containers. Each item in the pool has one container instrumented with ASAN and one for coverage"""
 
-    def __init__(self, benchmark: Benchmark, num_exp: int, num_evals: int):
-        self.lock = Lock()  # Works across processes when created before fork
-        self.containers = []
-        logging.info(
-            "Initializing %d container pairs for the trial", num_exp * num_evals
-        )
-        for _ in range(num_exp * num_evals):
-            self.containers.append(self.create_container_pair(benchmark))
+    def __init__(self, benchmark: Benchmark, num_cores: int):
+        manager = Manager()
+        self.containers = manager.Queue()
+        logger.info("Initializing %d container pairs for the trial", num_cores)
+        for _ in range(num_cores):
+            self.containers.put(self.create_container_pair(benchmark))
 
     def create_container_pair(self, benchmark: Benchmark) -> ContainerPair:
         address = ProjectContainerTool(benchmark=benchmark)
@@ -36,18 +34,11 @@ class ContainerPool:
         coverage = ProjectContainerTool(benchmark=benchmark)
         oss_fuzz_checkout.ENABLE_CACHING = True
 
-        in_use = Value("b", False)
-
-        return ContainerPair(address, coverage, in_use)
+        return ContainerPair(address, coverage)
 
     def get_container_pair(self) -> Optional[ContainerPair]:
-        with self.lock:
-            for pair in self.containers:
-                if not pair.in_use.value:
-                    pair.in_use.value = True
-                    return pair
-        return None
+        return self.containers.get()
 
     def release_container_pair(self, pair: ContainerPair):
-        with self.lock:
-            pair.in_use.value = False
+        self.containers.put(pair)
+    
